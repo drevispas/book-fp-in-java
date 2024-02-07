f45be30b0e44b5ebdd2d0a1af66d184bfafbbaea
Brad (김용건) <yonggeon.kim@gmail.com>
2024-02-07 16:41:17 +0900

    solve: refactoring/fpij/FactorialTest (#2)

--- src/main/java/org/example/fpij/refactoring/Factorial.java
+++ src/main/java/org/example/fpij/refactoring/Factorial.java
@@ -1,14 +1,15 @@
 package org.example.fpij.refactoring;
 
 import java.math.BigInteger;
+import java.util.stream.LongStream;
 
 public class Factorial {
 
   public BigInteger compute(long upTo) {
-    BigInteger result = BigInteger.ONE;
-    for(int i=1;i<=upTo;i++){
-      result = result.multiply(BigInteger.valueOf(i));
-    }
-    return result;
+    return BigInteger.valueOf(
+        LongStream.rangeClosed(1, upTo)
+            .reduce((x, y) -> x * y)
+            .orElse(0)
+    );
   }
 }
