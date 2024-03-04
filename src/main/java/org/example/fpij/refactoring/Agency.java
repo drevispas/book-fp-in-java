60140721fa4135cb588e5fba99244b67f05f3786
김용건 <yonggeon.kim@gmail.com>
2024-03-04 19:05:34 +0900

    Solve refactoring/fpij/Agent

--- src/main/java/org/example/fpij/refactoring/Agency.java
+++ src/main/java/org/example/fpij/refactoring/Agency.java
@@ -5,16 +5,18 @@ import java.util.Set;
 public class Agency {
 
   public boolean isChaperoneRequired(Set<Person> people) {
-    boolean required=true;
-    if (people.isEmpty()) required=false;
-    else {
-      for (var person:people) {
-        if (person.age() >= 18) {
-          required=false;
-          break;
-        }
-      }
-    }
-    return required;
+//    boolean required=true;
+//    if (people.isEmpty()) required=false;
+//    else {
+//      for (var person:people) {
+//        if (person.age() >= 18) {
+//          required=false;
+//          break;
+//        }
+//      }
+//    }
+//    return required;
+
+    return !people.isEmpty() && people.stream().noneMatch(p -> p.age()>=18);
   }
 }
